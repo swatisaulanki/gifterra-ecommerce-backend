d6214480d6d9be59be0ecaf41ac6dd6c8c25250c
swatisaulanki <swatisaulanki@gmail.com>
2025-05-28 16:58:05 +0530

    updated model

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,9 +12,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('❌ Error connecting to MySQL:', err.message);
+    console.error('Error connecting to MySQL:', err.message);
   } else {
-    console.log('✅ Connected to MySQL Database');
+    console.log('Connected to MySQL Database');
   }
 });
 
